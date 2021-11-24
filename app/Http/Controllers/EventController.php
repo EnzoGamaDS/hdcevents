907dc643e22ae84066a901005946434e8fa2c28c
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-11-24 13:25:58 -0300

    salvando imagens no servidor e o caminho no banco

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -30,9 +30,22 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+
+        //image upload 
+        if ($request ->hasFile('image') && $request->file('image')->isValid() ) {
+            $requestImage = $request->image;
+            
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now"). "." . $extension);
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+        }
         
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg','O evento foi criado com sucsso !');
     }
 }
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -5,8 +5,13 @@
 @section('content')
     <div id="event-create-container" class="col-md-6 offset-md-3">
         <h1>Crie seu evento</h1>
-        <form action="/events" method="POST">
+        <form action="/events" method="POST" enctype="multipart/form-data">
             @csrf
+            <!--baixando imagens, obs:apenas o caminho será salvo no banco-->
+            <div class="form-group">
+                <label for="image">Imagem do evento:</label>
+                <input type="file" id="image" name="image" class="form-control-file">
+            </div>
             <div class="form-group">
                 <label for="title">Eventos:</label>
                 <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">

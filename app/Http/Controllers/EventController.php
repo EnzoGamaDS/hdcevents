d64a0b03c10ec7b295395b2a1807d9b9b01d48ef
EnzoGamaDS <enzo.santos@ucsal.edu.br>
2021-12-01 20:21:03 -0300

    mostrando dono do evento

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -70,7 +71,9 @@ class EventController extends Controller
     }
     public function show($id){
         $event = Event::findOrFail($id);
+
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
         
-        return view('events.show', ['event'=> $event]);
+        return view('events.show', ['event'=> $event, 'eventOwner'=> $eventOwner]);
     }
 }
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -13,7 +13,7 @@
                 <h1>{{$event["title"]}}</h1>
                 <p class="event-city"><ion-icon name="location-outline"></ion-icon>{{$event["city"]}}</p>
                 <p class="events-participants"><ion-icon name="people-outline"></ion-icon>x participantes</p>
-                <p class="events-owner"><ion-icon name="star-outline"></ion-icon>Dono do evento</p>
+                <p class="events-owner"><ion-icon name="star-outline"></ion-icon>{{$eventOwner['name']}}</p>
                 <a href="#" class="btn btn-primary" id="event-submit" > confirmar presença</a>
                 <h3>O evento conta com :</h3>
                 <ul id="itens-list">
